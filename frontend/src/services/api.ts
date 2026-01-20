4c411c926fe6a2b03b1bfc1c54f48c0d7892c8f8
Marek Honc <marek.honc@outlook.com>
2026-01-20 16:32:16 +0100

    Url test

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -10,7 +10,7 @@ import {
   Tag,
 } from '../types';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://car-listing-backend-r37s.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
